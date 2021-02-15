e7cf9cea09f902bc4277d2433c79ec0770649a98
brenaudon <brenaudon@users.noreply.github.com>
2021-02-15 23:24:31 +0100

    Sticky menu Accueil

--- js/menu.js
+++ js/menu.js
@@ -12,26 +12,25 @@ var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if(scroll_menu != null){
-    if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > sticky) {
       header.classList.add("sticky");
-      scroll_menu.classList.add("sticky");
       haut.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
-      scroll_menu.classList.remove("sticky");
       haut.classList.remove("sticky");
+    }
+  if(scroll_menu != null){
+    if (window.pageYOffset > sticky) {
+      scroll_menu.classList.add("sticky");
+    } else {
+      scroll_menu.classList.remove("sticky");
     }
   }
   if(scroll_menu_projets != null){
     if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
       scroll_menu_projets.classList.add("sticky");
-      haut.classList.add("sticky");
     } else {
-      header.classList.remove("sticky");
       scroll_menu_projets.classList.remove("sticky");
-      haut.classList.remove("sticky");
     }
   }
   
